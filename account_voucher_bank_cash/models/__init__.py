8d98b9004ec4914adce334f1057eb6c59962fafb
Andhitia Rama <andhitia.r@gmail.com>
2017-11-30 04:42:26 +0700

    [8.0.0.0.0] account_voucher_bank_cash

---
+++ account_voucher_bank_cash/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    bank_voucher,
+    bank_receipt,
+    bank_payment,
+    cash_receipt,
+    cash_payment
+)
