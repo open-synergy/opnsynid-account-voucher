221c5a9a9f01f21f7931d7af1aa6eb1c452c1f11
Andhitia Rama <andhitia.r@gmail.com>
2018-01-04 20:24:19 +0700

    [8.0.1.0.0] account_voucher_giro

---
+++ account_voucher_giro/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Giro Accounting Voucher",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_voucher_bank_cash",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/account_voucher_type_data.xml",
+        "views/account_giro_voucher_views.xml",
+        "views/account_giro_receipt_views.xml",
+        "views/account_giro_payment_views.xml",
+    ],
+}
---
+++ account_voucher_giro/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    giro_voucher,
+    giro_receipt,
+    giro_payment,
+)
