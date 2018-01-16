5f96bc9d9b41e6761e91572ede9e7137b7bfb31b
Andhitia Rama <andhitia.r@gmail.com>
2018-01-17 06:31:31 +0700

    [8.0.4.2.0] account_voucher_common
    * Move line import mechanism

--- account_voucher_common/__openerp__.py
+++ account_voucher_common/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Common Accounting Voucher Feature",
-    "version": "8.0.4.1.0",
+    "version": "8.0.4.2.0",
     "category": "Accounting",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
@@ -15,6 +15,7 @@
     "data": [
         "security/ir.model.access.csv",
         "wizards/wizard_post_voucher.xml",
+        "wizards/wizard_import_move_line.xml",
         "views/account_voucher_type_views.xml",
         "views/account_voucher_common_views.xml",
     ],
