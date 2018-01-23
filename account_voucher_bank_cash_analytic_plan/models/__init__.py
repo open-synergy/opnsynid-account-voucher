d1f3c5a998322907e592f9efec2c85d01b17aa8d
mike <viriyananda.michael@gmail.com>
2018-01-23 17:24:15 +0700

    [8.0.1.0.0] account_voucher_bank_cash_analytic_plan

---
+++ account_voucher_bank_cash_analytic_plan/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    bank_payment,
+    bank_receipt,
+    cash_payment,
+    cash_receipt)
