db5ba97ce5cad5f985313554ec1280f15b20e2f7
mike <viriyananda.michael@gmail.com>
2018-01-23 17:24:29 +0700

    [8.0.1.0.0] account_voucher_cheque_analytic_plan

---
+++ account_voucher_cheque_analytic_plan/models/cheque_receipt.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models
+
+
+class ChequeReceiptLine(models.Model):
+    _name = "account.cheque_receipt_line"
+    _inherit = "account.voucher_line_common"
+
+
+class ChequeReceiptLineTax(models.Model):
+    _name = "account.cheque_receipt_line_tax"
+    _inherit = "account.voucher_line_tax_common"
