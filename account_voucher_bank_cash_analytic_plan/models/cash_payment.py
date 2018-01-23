f1a948ea49b78ec292fdfd06b5e19110885479d3
mike <viriyananda.michael@gmail.com>
2018-01-23 17:24:15 +0700

    Adding new module account_voucher_bank_cash_analytic_plan

---
+++ account_voucher_bank_cash_analytic_plan/models/cash_payment.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models
+
+
+class CashPaymentLine(models.Model):
+    _name = "account.cash_payment_line"
+    _inherit = "account.voucher_line_common"
+
+class CashPaymentLineTax(models.Model):
+    _name = "account.cash_payment_line_tax"
+    _inherit = "account.voucher_line_tax_common"
