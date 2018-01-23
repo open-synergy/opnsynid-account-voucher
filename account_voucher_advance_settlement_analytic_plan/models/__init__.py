5c0b8ed40dc31b57803b47eaddd918a3c203f87b
mike <viriyananda.michael@gmail.com>
2018-01-23 17:24:00 +0700

    Adding new module account_voucher_advance_settlement_analytic_plan

---
+++ account_voucher_advance_settlement_analytic_plan/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    purchase_advance_settlement,
+    sale_advance_settlement)
