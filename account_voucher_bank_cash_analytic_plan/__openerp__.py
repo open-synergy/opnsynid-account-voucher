5d7b33f7b980277e0485686cb4e8855e9b8a8ba8
mike <viriyananda.michael@gmail.com>
2018-01-23 17:24:15 +0700

    [8.0.1.0.0] account_voucher_bank_cash_analytic_plan

---
+++ account_voucher_bank_cash_analytic_plan/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Bank & Cash Accounting Voucher Analytic Plan Feature",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "account_voucher_bank_cash",
+        "account_voucher_common_analytic_plan"
+    ],
+    "data": [
+        "views/account_bank_payment_views.xml",
+        "views/account_bank_receipt_views.xml",
+        "views/account_cash_payment_views.xml",
+        "views/account_cash_receipt_views.xml",
+    ],
+}
