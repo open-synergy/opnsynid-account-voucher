0c4a6c07d55ef7e225ea708cc386a1651294c82f
mike <viriyananda.michael@gmail.com>
2018-01-23 13:19:48 +0700

    [8.0.1.0.0] account_voucher_common_analytic_plan

---
+++ account_voucher_common_analytic_plan/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Common Accounting Voucher Analytic Plan Feature",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_analytic_plans",
+        "account_voucher_common"
+    ],
+    "auto_install": True,
+}
