ec531f40b00fb0653ce81d7512cd5da315d0e981
mike <viriyananda.michael@gmail.com>
2018-01-24 18:42:39 +0700

    Adding new module account_voucher_common_start_end_date

---
+++ account_voucher_common_start_end_date/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    voucher_line_common,
+    voucher_line_tax_common)
