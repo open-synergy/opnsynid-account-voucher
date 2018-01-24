858a267054d14fc5f17c3f4b219700ef3bad3da8
mike <viriyananda.michael@gmail.com>
2018-01-24 18:42:59 +0700

    Adding new module account_voucher_giro_start_end_date

---
+++ account_voucher_giro_start_end_date/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Giro Accounting Voucher Start & End Date Feature",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "account_voucher_giro",
+        "account_voucher_common_start_end_date"
+    ],
+    "data": [
+        "views/account_giro_receipt_views.xml",
+        "views/account_giro_payment_views.xml",
+    ],
+}
---
+++ account_voucher_giro_start_end_date/models/giro_payment.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models
+
+
+class GiroPaymentLine(models.Model):
+    _name = "account.giro_payment_line"
+    _inherit = [
+        "account.giro_payment_line",
+        "account.voucher_line_common"
+    ]
+
+class GiroPaymentLineTax(models.Model):
+    _name = "account.giro_payment_line_tax"
+    _inherit = [
+        "account.giro_payment_line_tax",
+        "account.voucher_line_tax_common"
+    ]
