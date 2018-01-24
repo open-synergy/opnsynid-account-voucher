d49af49cdadcbe7ba1b0cb362968ec5755e38334
mike <viriyananda.michael@gmail.com>
2018-01-24 18:42:39 +0700

    Adding new module account_voucher_common_start_end_date

---
+++ account_voucher_common_start_end_date/models/voucher_line_tax_common.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api
+
+
+class VoucherLineTaxCommon(models.AbstractModel):
+    _inherit = "account.voucher_line_tax_common"
+
+    @api.multi
+    def _prepare_move_line(self):
+        res = super(VoucherLineTaxCommon, self)._prepare_move_line()
+        vline = self.voucher_line_id
+        res["start_date"] =\
+            vline.date_start or \
+            False
+        res["end_date"] =\
+            vline.date_end or \
+            False
+        return res
