f1612987ae140724018b7d2a83a0d9f50354353f
Andhitia Rama <andhitia.r@gmail.com>
2018-03-25 12:51:37 +0700

    [8.0.1.0.0] account_voucher_cheque_analytic_default

---
+++ account_voucher_cheque_analytic_default/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Cheque Accounting Voucher Integration With Analytic Default",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "account_voucher_cheque",
+        "account_voucher_common_analytic_default"
+    ],
+    "data": [
+    ],
+}
---
+++ account_voucher_cheque_analytic_default/models/cheque_payment.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models
+
+
+class ChequePaymentLine(models.Model):
+    _name = "account.cheque_payment_line"
+    _inherit = [
+        "account.cheque_payment_line",
+        "account.voucher_line_common"
+    ]
