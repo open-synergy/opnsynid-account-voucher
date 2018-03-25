b74c3a9ac5a5b2f263b6f5b6394d2f31c9e37f48
Andhitia Rama <andhitia.r@gmail.com>
2018-03-25 12:12:31 +0700

    [8.0.1.0.0] account_voucher_bank_cash_analytic_default

---
+++ account_voucher_bank_cash_analytic_default/models/bank_receipt.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models
+
+
+class BankReceiptLine(models.Model):
+    _name = "account.bank_receipt_line"
+    _inherit = [
+        "account.bank_receipt_line",
+        "account.voucher_line_common"
+    ]
