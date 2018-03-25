f7f4280ec327e96f865ab34700eba86a1040889e
Andhitia Rama <andhitia.r@gmail.com>
2018-03-25 12:38:36 +0700

    [8.0.1.0.0] account_voucher_giro_analytic_default

---
+++ account_voucher_giro_analytic_default/models/giro_receipt.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models
+
+
+class GiroReceiptLine(models.Model):
+    _name = "account.giro_receipt_line"
+    _inherit = [
+        "account.giro_receipt_line",
+        "account.voucher_line_common"
+    ]
