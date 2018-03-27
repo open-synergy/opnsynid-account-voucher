7c0a57ba31370be68b27b0bf41a6e3467d7d050a
Andhitia Rama <andhitia.r@gmail.com>
2018-03-27 20:51:05 +0700

    [8.0.1.2.0] account_voucher_common_analytic_plan

--- account_voucher_common_analytic_plan/models/voucher_line_common.py
+++ account_voucher_common_analytic_plan/models/voucher_line_common.py
@@ -8,10 +8,36 @@ from openerp import models, fields, api
 class VoucherLineCommon(models.AbstractModel):
     _inherit = "account.voucher_line_common"
 
+    @api.model
+    def _default_analytic_plan_account_id(self):
+        partner_id = self.env.context.get("default_partner_id", False)
+        analytic_default = self._get_analytic_default(
+            partner_id=partner_id, user_id=self.env.user.id)
+        return analytic_default and analytic_default.analytics_id
+
     analytic_plan_account_id = fields.Many2one(
         string="Analytic Plan",
         comodel_name="account.analytic.plan.instance"
     )
+
+    @api.onchange(
+        "partner_id",
+        "product_id",
+    )
+    def onchange_plan_account_id(self):
+        analytic_default = self._get_analytic_default(
+            partner_id=self.partner_id and self.partner_id.id or False,
+            product_id=self.product_id and self.product_id.id or False,
+            user_id=self.env.user.id,
+        )
+        self.analytic_plan_account_id = analytic_default.analytics_id
+
+    @api.onchange(
+        "partner_id",
+        "product_id",
+    )
+    def onchange_analytic_account_id(self):
+        self.analytic_account_id = False
 
     @api.multi
     def _prepare_move_line(self):
