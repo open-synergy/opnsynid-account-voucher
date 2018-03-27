843926a80064d2470dc1a20b0a5f84dae269df9e
Andhitia Rama <andhitia.r@gmail.com>
2018-03-27 21:19:31 +0700

    [8.0.5.1.1] account_voucher_common

--- account_voucher_common/__openerp__.py
+++ account_voucher_common/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Common Accounting Voucher Feature",
-    "version": "8.0.5.1.0",
+    "version": "8.0.5.1.1",
     "category": "Accounting",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
