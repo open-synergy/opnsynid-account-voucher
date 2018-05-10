fe3e9262a42e181cbf90bc335660bb5653dec044
Andhitia Rama <andhitia.r@gmail.com>
2018-05-10 10:48:35 +0700

    [8.0.1.0.0] account_voucher_refund_settlement_analytic_plan

---
+++ account_voucher_refund_settlement_analytic_plan/models/purchase_refund_settlement.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models
+
+
+class PurchaseRefundSettlementLine(models.Model):
+    _name = "account.purchase_refund_settlement_line"
+    _inherit = [
+        "account.purchase_refund_settlement_line",
+        "account.voucher_line_common"
+    ]
+
+
+class PurchaseRefundSettlementLineTax(models.Model):
+    _name = "account.purchase_refund_settlement_line_tax"
+    _inherit = [
+        "account.purchase_refund_settlement_line_tax",
+        "account.voucher_line_tax_common"
+    ]
