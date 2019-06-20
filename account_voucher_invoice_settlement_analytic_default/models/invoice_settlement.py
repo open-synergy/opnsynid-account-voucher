71f58e830f6f1f022a4c0f0a0158fb1c07e07b68
Andhitia Rama <andhitia.r@gmail.com>
2019-06-20 21:33:08 +0700

    [8.0.1.0.0] account_voucher_invoice_settlement_analytic_default

---
+++ account_voucher_invoice_settlement_analytic_default/models/invoice_settlement.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models
+
+
+class InvoiceSettlementLine(models.Model):
+    _name = "account.invoice_settlement_line"
+    _inherit = [
+        "account.invoice_settlement_line",
+        "account.voucher_line_common"
+    ]
