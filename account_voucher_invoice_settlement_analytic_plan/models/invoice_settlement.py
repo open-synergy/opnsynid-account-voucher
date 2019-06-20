fae216897b268fe32fbbd167770fefda98c2885d
Andhitia Rama <andhitia.r@gmail.com>
2019-06-20 22:03:06 +0700

    [8.0.1.0.0] account_voucher_invoice_settlement_analytic_plan

---
+++ account_voucher_invoice_settlement_analytic_plan/models/invoice_settlement.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models
+
+
+class InvoiceSettlementSettlementLine(models.Model):
+    _name = "account.invoice_settlement_line"
+    _inherit = [
+        "account.invoice_settlement_line",
+        "account.voucher_line_common"
+    ]
+
+
+class InvoiceSettlementSettlementLineTax(models.Model):
+    _name = "account.invoice_settlement_line_tax"
+    _inherit = [
+        "account.invoice_settlement_line_tax",
+        "account.voucher_line_tax_common"
+    ]
