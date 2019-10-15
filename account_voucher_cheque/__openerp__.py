8dbe74b22c03800e0bca430455dff1cb9b852b4a
mikevhe18 <viriyananda.michael@gmail.com>
2019-10-15 19:59:56 +0700

    [8.0.1.5.0][account_voucher_cheque]

--- account_voucher_cheque/__openerp__.py
+++ account_voucher_cheque/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Cheque Accounting Voucher",
-    "version": "8.0.1.4.0",
+    "version": "8.0.1.5.0",
     "category": "Accounting",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
@@ -15,6 +15,11 @@
     "data": [
         "security/ir.model.access.csv",
         "data/account_voucher_type_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/base_sequence_configurator_data.xml",
+        "data/base_workflow_policy_cheque_payment_data.xml",
+        "data/base_workflow_policy_cheque_receipt_data.xml",
+        "data/base_cancel_reason_configurator_data.xml",
         "views/account_cheque_voucher_views.xml",
         "views/account_cheque_receipt_views.xml",
         "views/account_cheque_payment_views.xml",
