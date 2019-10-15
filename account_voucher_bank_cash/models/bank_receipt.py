1eb362087d56c61a85396b1ad2e7a9df48215efb
mikevhe18 <viriyananda.michael@gmail.com>
2019-10-15 19:58:28 +0700

    [8.0.1.8.0][account_voucher_bank_cash]

--- account_voucher_bank_cash/models/bank_receipt.py
+++ account_voucher_bank_cash/models/bank_receipt.py
@@ -5,15 +5,15 @@
 from openerp import models, fields, api
 
 
-class BankPayment(models.Model):
-    _name = "account.bank_payment"
+class BankReceipt(models.Model):
+    _name = "account.bank_receipt"
     _inherit = "account.bank_voucher"
-    _description = "Bank Payment"
+    _description = "Bank Receipt"
 
     @api.model
     def _default_type_id(self):
         return self.env.ref(
-            "account_voucher_bank_cash.voucher_type_bank_payment").id
+            "account_voucher_bank_cash.voucher_type_bank_receipt").id
 
     type_id = fields.Many2one(
         default=lambda self: self._default_type_id(),
@@ -26,26 +26,26 @@ class BankPayment(models.Model):
     )
 
     line_ids = fields.One2many(
-        comodel_name="account.bank_payment_line",
+        comodel_name="account.bank_receipt_line",
     )
     line_dr_ids = fields.One2many(
-        comodel_name="account.bank_payment_line",
+        comodel_name="account.bank_receipt_line",
     )
     line_cr_ids = fields.One2many(
-        comodel_name="account.bank_payment_line",
+        comodel_name="account.bank_receipt_line",
     )
 
 
-class BankPaymentLine(models.Model):
-    _name = "account.bank_payment_line"
+class BankReceiptLine(models.Model):
+    _name = "account.bank_receipt_line"
     _inherit = "account.voucher_line_common"
-    _description = "Bank Payment Line"
+    _description = "Bank Receipt Line"
 
     voucher_id = fields.Many2one(
-        comodel_name="account.bank_payment",
+        comodel_name="account.bank_receipt",
     )
     tax_ids = fields.One2many(
-        comodel_name="account.bank_payment_line_tax",
+        comodel_name="account.bank_receipt_line_tax",
     )
     currency_id = fields.Many2one(
         comodel_name="res.currency",
@@ -59,13 +59,13 @@ class BankPaymentLine(models.Model):
     )
 
 
-class BankPaymentLineTax(models.Model):
-    _name = "account.bank_payment_line_tax"
+class BankReceiptLineTax(models.Model):
+    _name = "account.bank_receipt_line_tax"
     _inherit = "account.voucher_line_tax_common"
-    _description = "Bank Payment Line Tax"
+    _description = "Bank Receipt Line Tax"
 
     voucher_line_id = fields.Many2one(
-        comodel_name="account.bank_payment_line",
+        comodel_name="account.bank_receipt_line",
     )
     currency_id = fields.Many2one(
         comodel_name="res.currency",
