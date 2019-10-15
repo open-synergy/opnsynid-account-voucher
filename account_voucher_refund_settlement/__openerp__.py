37066b82296c1b131b98977e1ae8b145e3149f0c
mikevhe18 <viriyananda.michael@gmail.com>
2019-10-15 20:01:15 +0700

    [8.0.1.1.0][account_voucher_refund_settlement]

--- account_voucher_refund_settlement/__openerp__.py
+++ account_voucher_refund_settlement/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Refund Settlement Voucher",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "category": "Accounting",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
@@ -15,6 +15,11 @@
     "data": [
         "security/ir.model.access.csv",
         "data/account_voucher_type_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/base_sequence_configurator_data.xml",
+        "data/base_workflow_policy_sale_refund_data.xml",
+        "data/base_workflow_policy_purchase_refund_data.xml",
+        "data/base_cancel_reason_configurator_data.xml",
         "views/account_sale_refund_settlement_views.xml",
         "views/account_purchase_refund_settlement_views.xml",
     ],
