4bc51fbb5aa8b0aa3f05b88309c1baaf01b36696
mikevhe18 <viriyananda.michael@gmail.com>
2020-06-17 18:42:01 +0700

    [8.0.2.0.0][account_voucher_advance_settlement]

--- account_voucher_advance_settlement/models/__init__.py
+++ account_voucher_advance_settlement/models/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from . import (
     sale_advance_settlement,
     purchase_advance_settlement,
+    tier_definition,
 )
