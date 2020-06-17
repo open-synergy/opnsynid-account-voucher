d93ad6a157af329d93c90a45631e997fd00ff8be
mikevhe18 <viriyananda.michael@gmail.com>
2020-06-17 18:41:31 +0700

    [8.0.2.0.0][account_voucher_cheque]

--- account_voucher_cheque/models/__init__.py
+++ account_voucher_cheque/models/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from . import (
     cheque_voucher,
     cheque_receipt,
     cheque_payment,
+    tier_definition,
 )
