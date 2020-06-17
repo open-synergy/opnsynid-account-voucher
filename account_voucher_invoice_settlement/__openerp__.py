a06f110228334f2659198bb8ff9e10fc18d2536b
mikevhe18 <viriyananda.michael@gmail.com>
2020-06-17 18:42:28 +0700

    [8.0.2.0.0][account_voucher_invoice_settlement]

--- account_voucher_invoice_settlement/__openerp__.py
+++ account_voucher_invoice_settlement/__openerp__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Invoice Settlement Voucher",
-    "version": "8.0.1.1.0",
+    "version": "8.0.2.0.0",
     "category": "Accounting",
-    "website": "https://opensynergy-indonesia.com/",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
@@ -20,5 +21,8 @@
         "data/base_workflow_policy_invoice_settlement_data.xml",
         "data/base_cancel_reason_configurator_data.xml",
         "views/account_invoice_settlement_views.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
     ],
 }
