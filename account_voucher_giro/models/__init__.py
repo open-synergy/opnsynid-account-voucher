b63c5684da81f920860e229a05918476594514fd
mikevhe18 <viriyananda.michael@gmail.com>
2020-06-17 18:41:42 +0700

    [8.0.2.0.0][account_voucher_giro]

--- account_voucher_giro/models/__init__.py
+++ account_voucher_giro/models/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from . import (
     giro_voucher,
     giro_receipt,
     giro_payment,
+    tier_definition,
 )
