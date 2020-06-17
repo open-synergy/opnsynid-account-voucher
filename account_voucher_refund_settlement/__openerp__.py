f46fab000418166bb2cef9ffec2095d1eee30416
mikevhe18 <viriyananda.michael@gmail.com>
2020-06-17 18:42:15 +0700

    [8.0.2.0.0][account_voucher_refund_settlement]

--- account_voucher_refund_settlement/__openerp__.py
+++ account_voucher_refund_settlement/__openerp__.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Refund Settlement Voucher",
-    "version": "8.0.1.1.0",
+    "version": "8.0.2.0.0",
     "category": "Accounting",
-    "website": "https://opensynergy-indonesia.com/",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
