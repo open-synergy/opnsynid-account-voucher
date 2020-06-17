eda73d49ba5673a2c79fef14d7edfcef78ab2890
mikevhe18 <viriyananda.michael@gmail.com>
2020-06-17 18:40:56 +0700

    [8.0.7.0.0][account_voucher_common]
    * Multiple approval implementation
    * Change branding into Simetri Sinergi

---
+++ account_voucher_common/migrations/8.0.7.0.0/pre-migration.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+
+def migrate(cr, version):
+    if not version:
+        return
+    if openupgrade.table_exists(cr, "rel_vtype_journal_approve_group"):
+        openupgrade.logged_query(
+            cr,
+            """
+            DROP TABLE rel_vtype_journal_approve_group CASCADE
+            """)
