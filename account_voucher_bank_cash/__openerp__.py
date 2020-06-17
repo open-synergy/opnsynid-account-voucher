244678ab7a28d65f744ad105305ecf92defb6cf7
mikevhe18 <viriyananda.michael@gmail.com>
2020-06-17 18:41:12 +0700

    [8.0.2.0.0][account_voucher_bank_cash]

--- account_voucher_bank_cash/__openerp__.py
+++ account_voucher_bank_cash/__openerp__.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Bank & Cash Accounting Voucher",
-    "version": "8.0.1.8.0",
+    "version": "8.0.2.0.0",
     "category": "Accounting",
-    "website": "https://opensynergy-indonesia.com/",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
@@ -29,5 +31,8 @@
         "views/account_bank_payment_views.xml",
         "views/account_cash_payment_views.xml",
         "views/account_cash_receipt_views.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
     ],
 }
