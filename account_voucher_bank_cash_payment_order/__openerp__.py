5131f0d982c9e8db1ce3fd5bab3f92ede295f0bf
Andhitia Rama <andhitia.r@gmail.com>
2020-07-13 00:57:23 +0700

    [8.0.1.0.2] account_voucher_bank_cash_payment_order
    * Fix payment order creation

--- account_voucher_bank_cash_payment_order/__openerp__.py
+++ account_voucher_bank_cash_payment_order/__openerp__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Bank & Cash Accounting Voucher - Integration with Payment Order",
-    "version": "8.0.1.0.1",
+    "version": "8.0.1.0.2",
     "category": "Accounting",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
