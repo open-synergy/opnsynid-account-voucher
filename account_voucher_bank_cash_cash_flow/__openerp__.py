1183e9cdd1cbabbd92b7524a35201134295b2642
mikevhe18 <viriyananda.michael@gmail.com>
2020-07-28 17:12:12 +0700

    [8.0.1.0.0][account_voucher_bank_cash_cash_flow]

---
+++ account_voucher_bank_cash_cash_flow/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Bank & Cash Accounting Voucher - Cash Flow",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "account_voucher_bank_cash",
+        "account_cash_flow_code",
+    ],
+    "data": [
+        "views/account_cash_receipt_views.xml",
+        "views/account_bank_receipt_views.xml",
+        "views/account_cash_payment_views.xml",
+        "views/account_bank_payment_views.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
