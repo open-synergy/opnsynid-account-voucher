eb89a9f0d745c0b5aa180f7ec72bf90161f36d35
mikevhe18 <viriyananda.michael@gmail.com>
2020-08-03 09:38:19 +0700

    [8.0.1.0.0][account_voucher_bank_cash_cash_flow] Revision

--- account_voucher_bank_cash_cash_flow/models/__init__.py
+++ account_voucher_bank_cash_cash_flow/models/__init__.py
@@ -3,12 +3,6 @@
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from . import (
-    # bank_voucher,
-    # bank_receipt,
-    # bank_payment,
-    # cash_receipt,
-    # cash_payment,
-    voucher_line_common,
     bank_receipt_line,
     cash_receipt_line,
     bank_payment_line,
--- account_voucher_bank_cash_cash_flow/models/bank_receipt_line.py
+++ account_voucher_bank_cash_cash_flow/models/bank_receipt_line.py
@@ -3,12 +3,39 @@
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models
+from openerp import models, fields, api
 
 
 class BankReceiptLine(models.Model):
-    _name = "account.bank_receipt_line"
-    _inherit = [
-        "account.bank_receipt_line",
-        "account.voucher_line_common"
-    ]
+    _inherit = "account.bank_receipt_line"
+
+    direct_cash_flow_id = fields.Many2one(
+        string="Direct Cash Flow",
+        comodel_name="account.cash_flow_code",
+        domain=[
+            ("type", "=", "direct"),
+        ],
+    )
+    indirect_cash_flow_id = fields.Many2one(
+        string="Indirect Cash Flow",
+        comodel_name="account.cash_flow_code",
+        domain=[
+            ("type", "=", "indirect"),
+        ],
+    )
+
+    @api.multi
+    def _prepare_move_line(self):
+        _super = super(BankReceiptLine, self)
+        res = _super._prepare_move_line()
+        res["direct_cash_flow_id"] = self.direct_cash_flow_id.id
+        res["indirect_cash_flow_id"] = self.indirect_cash_flow_id.id
+        return res
+
+    @api.multi
+    def _prepare_exchange_move_line(self):
+        _super = super(BankReceiptLine, self)
+        res = _super._prepare_exchange_move_line()
+        res["direct_cash_flow_id"] = self.direct_cash_flow_id.id
+        res["indirect_cash_flow_id"] = self.indirect_cash_flow_id.id
+        return res
