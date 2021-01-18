89e802cad9feba056826380cbd37e9793fe39dc3
mikevhe18 <viriyananda.michael@gmail.com>
2021-01-18 19:55:05 +0700

    [account_voucher_common] 8.0.7.0.1

--- account_voucher_common/__openerp__.py
+++ account_voucher_common/__openerp__.py
@@ -5,7 +5,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Common Accounting Voucher Feature",
-    "version": "8.0.7.0.0",
+    "version": "8.0.7.0.1",
     "category": "Accounting",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
