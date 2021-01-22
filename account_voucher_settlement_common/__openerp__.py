8753de2cae551bfa56edd8673e91847606f17c88
mikevhe18 <viriyananda.michael@gmail.com>
2021-01-22 11:05:42 +0700

    [account_voucher_settlement_common] 8.0.1.3.1

--- account_voucher_settlement_common/__openerp__.py
+++ account_voucher_settlement_common/__openerp__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Settlement Voucher Common Feature",
-    "version": "8.0.1.3.0",
+    "version": "8.0.1.3.1",
     "category": "Accounting",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
