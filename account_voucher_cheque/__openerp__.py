2e67215bb168e2cee53caaeb782ae3741a307e84
mikevhe18 <viriyananda.michael@gmail.com>
2021-01-22 11:03:39 +0700

    [account_voucher_cheque] 8.0.2.0.1

--- account_voucher_cheque/__openerp__.py
+++ account_voucher_cheque/__openerp__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Cheque Accounting Voucher",
-    "version": "8.0.2.0.0",
+    "version": "8.0.2.0.1",
     "category": "Accounting",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
