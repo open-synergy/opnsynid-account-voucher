db3b7c4c8ba3772e07b95684c02b46fe27475f86
Nurazmi <azmimr67@gmail.com>
2021-06-17 07:53:28 +0700

    [account_voucher_refund_settlement] 8.0.2.1.0
    Fix #95

--- account_voucher_refund_settlement/__openerp__.py
+++ account_voucher_refund_settlement/__openerp__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 OpenSynergy Indonesia
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Refund Settlement Voucher",
-    "version": "8.0.2.0.1",
+    "version": "8.0.2.1.0",
     "category": "Accounting",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
@@ -15,7 +14,9 @@
     ],
     "data": [
         "security/ir.model.access.csv",
+        "menu.xml",
         "data/account_voucher_type_data.xml",
+        "data/ir_windows_action_data.xml",
         "data/ir_sequence_data.xml",
         "data/base_sequence_configurator_data.xml",
         "data/base_workflow_policy_sale_refund_data.xml",
