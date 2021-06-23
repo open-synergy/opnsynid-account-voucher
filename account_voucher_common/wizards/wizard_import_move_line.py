6bcb30a4caa640a7ce620d81c1b8559430a7a8d0
mikevhe18 <viriyananda.michael@gmail.com>
2021-06-23 18:28:42 +0700

    [8.0.7.6.0] account_voucher_common
    Fix #77

--- account_voucher_common/wizards/wizard_import_move_line.py
+++ account_voucher_common/wizards/wizard_import_move_line.py
@@ -1,9 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+import logging
+
+from openerp import api, fields, models
+from openerp.tools.safe_eval import safe_eval as eval
+
+_logger = logging.getLogger(__name__)
 
 
 class WizardImportMoveLine(models.TransientModel):
@@ -14,15 +18,10 @@ class WizardImportMoveLine(models.TransientModel):
     def default_get(self, fields):
         _super = super(WizardImportMoveLine, self)
         res = _super.default_get(fields)
-        active_model = self.env.context.get("active_model", False)
-        active_id = self.env.context.get("active_id", False)
-        import_type = self.env.context.get("import_type", False)
+        voucher = self._get_object()
 
-        voucher = self.env[active_model].browse([active_id])[0]
-
-        res["partner_id"] = voucher.partner_id and voucher.partner_id.id \
-            or False
-        res["import_type"] = import_type
+        res["partner_id"] = voucher.partner_id and voucher.partner_id.id or False
+        res["import_type"] = self.env.context.get("import_type", False)
 
         return res
 
@@ -45,9 +44,11 @@ class WizardImportMoveLine(models.TransientModel):
     )
 
     @api.onchange(
-        "partner_id", "import_type",
+        "partner_id",
+        "import_type",
     )
     def onchange_move_line_ids(self):
+        criteria = []
         result = {
             "domain": {
                 "move_line_ids": [("id", "=", 0)],
@@ -56,41 +57,94 @@ class WizardImportMoveLine(models.TransientModel):
         self.move_line_ids = False
 
         if self.partner_id and self.import_type:
-            commercial_partner_id =\
-                self.partner_id.commercial_partner_id.id
+            commercial_partner_id = self.partner_id.commercial_partner_id.id
 
             criteria = [
                 ("account_id.reconcile", "=", True),
                 ("reconcile_id", "=", False),
-                ("partner_id", "=", commercial_partner_id)
+                ("partner_id", "=", commercial_partner_id),
             ]
             if self.import_type == "dr":
                 criteria.append(("debit", ">", 0))
             else:
                 criteria.append(("credit", ">", 0))
-            result["domain"]["move_line_ids"] = criteria
+        voucher = self._get_object()
+        domain_type = self.get_domain_expression(voucher.type_id.python_code)
+        if domain_type:
+            if isinstance(domain_type, list):
+                if len(domain_type) > 1:
+                    for document in domain_type:
+                        criteria.append(document)
+                else:
+                    criteria.append(domain_type[0])
+
+        allowed_journal_ids = voucher.type_id.allowed_journal_ids
+        for allowed_journal in allowed_journal_ids:
+
+            domain_type_allowed_journal = self.get_domain_expression(
+                allowed_journal.python_code
+            )
+            if domain_type_allowed_journal:
+                if isinstance(domain_type_allowed_journal, list):
+                    if len(domain_type) > 1:
+                        for document in domain_type_allowed_journal:
+                            criteria.append(document)
+                    else:
+                        criteria.append(domain_type_allowed_journal[0])
+
+        result["domain"]["move_line_ids"] = criteria
         return result
 
     @api.multi
     def action_import_move_line(self):
         self.ensure_one()
-        active_model = self.env.context.get("active_model", False)
-        active_id = self.env.context.get("active_id", False)
-        voucher = self.env[active_model].browse([active_id])[0]
+        voucher = self._get_object()
         lines = []
 
         for move in self.move_line_ids:
             amount = move.amount_residual / voucher.exchange_rate
             line_type = self.import_type == "dr" and "cr" or "dr"
-            res = (0, 0, {
-                "name": move.name,
-                "partner_id": move.partner_id and move.partner_id.id or False,
-                "move_line_id": move.id,
-                "account_id": move.account_id.id,
-                "amount": amount,
-                "type": line_type,
-            })
+            res = (
+                0,
+                0,
+                {
+                    "name": move.name,
+                    "partner_id": move.partner_id and move.partner_id.id or False,
+                    "move_line_id": move.id,
+                    "account_id": move.account_id.id,
+                    "amount": amount,
+                    "type": line_type,
+                },
+            )
             lines.append(res)
-        voucher.write({
-            "line_ids": lines,
-        })
+        voucher.write(
+            {
+                "line_ids": lines,
+            }
+        )
+
+    @api.multi
+    def _get_object(self):
+        active_id = self.env.context.get("active_id", False)
+        active_model = self.env.context.get("active_model", "")
+        object = self.env[active_model].browse([active_id])[0]
+        return object
+
+    @api.multi
+    def _get_localdict(self):
+        return {
+            "env": self.env,
+            "rec": self._get_object(),
+        }
+
+    @api.multi
+    def get_domain_expression(self, python_condition):
+        result = []
+        localdict = self._get_localdict()
+        try:
+            eval(python_condition, localdict, mode="exec", nocopy=True)
+            result = localdict["result"]
+        except Exception as err:
+            _logger.info("Error: {}".format(err))
+
+        return result
