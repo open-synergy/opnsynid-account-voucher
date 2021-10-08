a62dd20e390e51034c2501995faa01768e1e4f0a
Andhitia Rama <andhitia.r@gmail.com>
2021-10-08 16:02:20 +0700

    pre-commit fix

--- account_voucher_cheque/models/cheque_payment.py
+++ account_voucher_cheque/models/cheque_payment.py
@@ -3,7 +3,7 @@
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+from openerp import api, fields, models
 
 
 class ChequePayment(models.Model):
@@ -13,8 +13,7 @@ class ChequePayment(models.Model):
 
     @api.model
     def _default_type_id(self):
-        return self.env.ref(
-            "account_voucher_cheque.voucher_type_cheque_payment").id
+        return self.env.ref("account_voucher_cheque.voucher_type_cheque_payment").id
 
     type_id = fields.Many2one(
         default=lambda self: self._default_type_id(),
--- account_voucher_cheque/models/cheque_receipt.py
+++ account_voucher_cheque/models/cheque_receipt.py
@@ -3,7 +3,7 @@
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+from openerp import api, fields, models
 
 
 class ChequeReceipt(models.Model):
@@ -13,8 +13,7 @@ class ChequeReceipt(models.Model):
 
     @api.model
     def _default_type_id(self):
-        return self.env.ref(
-            "account_voucher_cheque.voucher_type_cheque_receipt").id
+        return self.env.ref("account_voucher_cheque.voucher_type_cheque_receipt").id
 
     type_id = fields.Many2one(
         default=lambda self: self._default_type_id(),
