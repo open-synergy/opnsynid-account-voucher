130fa946434323d390ba29ab98974f230acbb309
mikevhe18 <viriyananda.michael@gmail.com>
2022-03-28 15:20:15 +0700

    [ssi_voucher_advance_settlement] 14.0.1.0.0

---
+++ ssi_voucher_advance_settlement/__manifest__.py
@@ -0,0 +1,38 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Advance Settlement Voucher",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_voucher_settlement_common",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "menu.xml",
+        "data/account_voucher_type_data.xml",
+        "data/ir_windows_action_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_purchase_settlement_data.xml",
+        "data/sequence_template_sale_settlement_data.xml",
+        "data/approval_template_purchase_settlement_data.xml",
+        "data/approval_template_sale_settlement_data.xml",
+        "data/policy_template_purchase_settlement_data.xml",
+        "data/policy_template_sale_settlement_data.xml",
+        "views/account_sale_advance_settlement_views.xml",
+        "views/account_purchase_advance_settlement_views.xml",
+    ],
+    "demo": [
+        "demo/account_journal_demo.xml",
+        "demo/account_voucher_type_allowed_journal_demo.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
