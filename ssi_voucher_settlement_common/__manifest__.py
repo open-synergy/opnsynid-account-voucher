f0a218f2dd24b30f98e5ae361eecd6b0a4f60734
mikevhe18 <viriyananda.michael@gmail.com>
2022-03-28 15:19:57 +0700

    [ssi_voucher_settlement_common] 14.0.1.0.0

---
+++ ssi_voucher_settlement_common/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Settlement Voucher Common",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_voucher_mixin",
+    ],
+    "data": [
+        "views/mixin_account_voucher_views.xml",
+    ],
+}
