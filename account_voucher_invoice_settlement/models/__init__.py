7ed3d05875a25e6f14424751e42c7255fc517fa9
mikevhe18 <viriyananda.michael@gmail.com>
2022-04-08 08:18:19 +0700

    [opnsynid-account-voucher] Renaming File * account_voucher_refund_settlement -> ssi_voucher_refund_settlement * account_voucher_invoice_settlement -> ssi_voucher_invoice_settlement * account_voucher_bank_cash -> ssi_voucher_bank_cash * account_voucher_giro -> ssi_voucher_giro * account_voucher_cheque -> ssi_voucher_cheque

--- account_voucher_invoice_settlement/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2019 OpenSynergy Indonesia
-# Copyright 2020 PT. Simetri Sinergi Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import (
-    invoice_settlement,
-    tier_definition,
-)
