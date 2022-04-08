f4d62d137ad7ce4cd55751f136314c5be205ac6c
mikevhe18 <viriyananda.michael@gmail.com>
2022-04-08 08:04:30 +0700

    [ssi_voucher_invoice_settlement] 14.0.1.0.0

---
+++ ssi_voucher_invoice_settlement/__manifest__.py
@@ -0,0 +1,33 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Invoice Settlement Voucher",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_voucher_settlement_common",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "menu.xml",
+        "data/account_voucher_type_data.xml",
+        "data/ir_windows_action_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "data/approval_template_data.xml",
+        "data/policy_template_data.xml",
+        "views/account_invoice_settlement_views.xml",
+    ],
+    "demo": [
+        "demo/account_journal_demo.xml",
+        "demo/account_voucher_type_allowed_journal_demo.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
---
+++ ssi_voucher_invoice_settlement/models/invoice_settlement.py
@@ -0,0 +1,79 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class InvoiceSettlement(models.Model):
+    _name = "account.invoice_settlement"
+    _inherit = "mixin.account.voucher"
+    _description = "Invoice Settlement"
+
+    @api.model
+    def _default_type_id(self):
+        return self.env.ref(
+            "ssi_voucher_invoice_settlement." "voucher_type_invoice_settlement"
+        ).id
+
+    type_id = fields.Many2one(
+        default=lambda self: self._default_type_id(),
+    )
+    company_currency_id = fields.Many2one(
+        string="Company Currency",
+        comodel_name="res.currency",
+        related="company_id.currency_id",
+        store=True,
+    )
+    line_ids = fields.One2many(
+        comodel_name="account.invoice_settlement_line",
+    )
+    line_dr_ids = fields.One2many(
+        comodel_name="account.invoice_settlement_line",
+    )
+    line_cr_ids = fields.One2many(
+        comodel_name="account.invoice_settlement_line",
+    )
+
+
+class InvoiceSettlementLine(models.Model):
+    _name = "account.invoice_settlement_line"
+    _inherit = "mixin.account.voucher.line"
+    _description = "Invoice Settlement Line"
+
+    voucher_id = fields.Many2one(
+        comodel_name="account.invoice_settlement",
+    )
+    tax_ids = fields.One2many(
+        comodel_name="account.invoice_settlement_line_tax",
+    )
+    currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        related="voucher_id.currency_id",
+        store=True,
+    )
+    company_currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        related="voucher_id.company_currency_id",
+        store=True,
+    )
+
+
+class InvoiceSettlementLineTax(models.Model):
+    _name = "account.invoice_settlement_line_tax"
+    _inherit = "mixin.account.voucher.line.tax"
+    _description = "Invoice Settlement Line Tax"
+
+    voucher_line_id = fields.Many2one(
+        comodel_name="account.invoice_settlement_line",
+    )
+    currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        related="voucher_line_id.currency_id",
+        store=True,
+    )
+    company_currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        related="voucher_line_id.company_currency_id",
+        store=True,
+    )
