b1913a3f94f97f876153da1ba7b9347d0dc5d3e1
Andhitia Rama <andhitia.r@gmail.com>
2022-04-09 17:01:00 +0700

    [ssi_voucher_bank_cash] 14.0.1.0.0 - QC
    * Fix monetary widget
    * Fix import move line button
    * Add amount voucher sum on tree
    * Remove payment mode
    * Fix menu and window actions name
    * Use standard # Document as name string

--- ssi_voucher_bank_cash/models/mixin_bank_voucher.py
+++ ssi_voucher_bank_cash/models/mixin_bank_voucher.py
@@ -2,42 +2,10 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import api, fields, models
+from odoo import models
 
 
 class MixinBankVoucher(models.AbstractModel):
     _name = "mixin.bank_voucher"
     _inherit = "mixin.account.voucher"
     _description = "Bank Voucher"
-
-    payment_mode_id = fields.Many2one(
-        string="Payment Mode",
-        comodel_name="payment.acquirer",
-        readonly=True,
-        states={
-            "draft": [
-                ("readonly", False),
-            ],
-        },
-    )
-
-    @api.onchange("journal_id")
-    def onchange_payment_mode_id(self):
-        result = {
-            "domain": {
-                "payment_mode_id": [
-                    ("id", "=", 0),
-                ]
-            },
-        }
-        if self.journal_id:
-            result["domain"]["payment_mode_id"] = [
-                ("journal_id", "=", self.journal_id.id)
-            ]
-
-            if self.payment_mode_id:
-                if self.payment_mode_id.journal != self.journal_id:
-                    self.payment_mode_id = False
-            else:
-                self.payment_mode_id = False
-        return result
