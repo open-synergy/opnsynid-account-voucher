cd9a38aa5be8140a6dfa4f0961e3ef8c84c683de
Andhitia Rama <andhitia.r@gmail.com>
2022-04-15 20:17:15 +0700

    [ssi_voucher_advance_settlement] 14.0.2.0.0
    * Assign menu into Financial Accounting root menu
    * Group implementation
    * Add more demo data

--- ssi_voucher_advance_settlement/__manifest__.py
+++ ssi_voucher_advance_settlement/__manifest__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Advance Settlement Voucher",
-    "version": "14.0.1.2.0",
+    "version": "14.0.2.0.0",
     "category": "Accounting",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
@@ -12,8 +12,11 @@
     "installable": True,
     "depends": [
         "ssi_voucher_settlement_common",
+        "ssi_financial_accounting",
     ],
     "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
         "security/ir.model.access.csv",
         "menu.xml",
         "data/account_voucher_type_data.xml",
