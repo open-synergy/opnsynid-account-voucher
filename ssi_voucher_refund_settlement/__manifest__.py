912a57696e3ec6eccdb8ead5568fc4641f138ea1
mikevhe18 <viriyananda.michael@gmail.com>
2022-05-13 13:35:46 +0700

    [ssi_voucher_refund_settlement] 14.0.2.1.0

--- ssi_voucher_refund_settlement/__manifest__.py
+++ ssi_voucher_refund_settlement/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Refund Settlement Voucher",
-    "version": "14.0.2.0.0",
+    "version": "14.0.2.1.0",
     "category": "Accounting",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
@@ -16,6 +16,7 @@
     "data": [
         "security/ir_module_category_data.xml",
         "security/res_group_data.xml",
+        "security/ir_rule_data.xml",
         "security/ir.model.access.csv",
         "menu.xml",
         "data/account_voucher_type_data.xml",
