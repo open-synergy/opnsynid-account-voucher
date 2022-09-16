716c53596723db25e9ce10a5870845ee3b27d5ff
Andhitia Rama <andhitia.r@gmail.com>
2022-09-17 06:25:58 +0700

    [ssi_voucher_invoice_settlement] 14.0.2.2.0
    * Add standard journal + add it on allowed journal

--- ssi_voucher_invoice_settlement/__manifest__.py
+++ ssi_voucher_invoice_settlement/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Invoice Settlement Voucher",
-    "version": "14.0.2.1.0",
+    "version": "14.0.2.2.0",
     "category": "Accounting",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
@@ -25,6 +25,8 @@
         "data/sequence_template_data.xml",
         "data/approval_template_data.xml",
         "data/policy_template_data.xml",
+        "data/account_journal_data.xml",
+        "data/account_voucher_type_allowed_journal_data.xml",
         "views/account_invoice_settlement_views.xml",
     ],
     "demo": [
