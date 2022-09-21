745b3db3c5a587bd6cf77a5df17dd5dfb1ae53ba
Andhitia Rama <andhitia.r@gmail.com>
2022-09-21 15:02:56 +0700

    [ssi_voucher_mixin] 14.0.1.4.0
    * Add wizard to add journal to voucher type

---
+++ ssi_voucher_mixin/models/account_journal.py
@@ -0,0 +1,24 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
+
+from odoo import models
+
+
+class AccountJournal(models.Model):
+    _name = "account.journal"
+    _inherit = "account.journal"
+
+    def add_to_voucher_type(self, voucher_type):
+        for record in self:
+            record._add_to_voucher_type(voucher_type)
+
+    def _add_to_voucher_type(self, voucher_type):
+        self.ensure_one()
+        AllowedJournal = self.env["account.voucher_type_allowed_journal"]
+        AllowedJournal.create(
+            {
+                "voucher_type_id": voucher_type.id,
+                "journal_id": self.id,
+            }
+        )
