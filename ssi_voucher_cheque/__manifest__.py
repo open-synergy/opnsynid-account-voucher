e1e54b1f825fe4dc8fce74b68f7d28511b0ce8f7
mikevhe18 <viriyananda.michael@gmail.com>
2023-08-21 21:04:05 +0700

    [ssi_voucher_cheque] Publish To PyPi

--- ssi_voucher_cheque/__manifest__.py
+++ ssi_voucher_cheque/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Cheque Accounting Voucher",
-    "version": "14.0.2.1.0",
+    "version": "14.0.2.1.1",
     "category": "Accounting",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
