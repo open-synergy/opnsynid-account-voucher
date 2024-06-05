ceb29d6daaed73a67505b1b2c03c9ee66504c6a8
mikevhe18 <viriyananda.michael@gmail.com>
2024-06-05 17:31:23 +0700

    [ADD] Depedencies to ssi_work_log_mixin

--- ssi_voucher_bank_cash/__manifest__.py
+++ ssi_voucher_bank_cash/__manifest__.py
@@ -13,6 +13,7 @@
         "ssi_voucher_mixin",
         "account_payment",
         "ssi_financial_accounting",
+        "ssi_work_log_mixin",
     ],
     "data": [
         "security/ir_module_category_data.xml",
