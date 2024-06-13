4eb6a351c30c7cd7ce0ebb325c19e2aaadcfd287
mikevhe18 <viriyananda.michael@gmail.com>
2024-06-13 10:21:17 +0700

    [ssi_voucher_mixin_work_log] 14.0.1.0.0

---
+++ ssi_voucher_mixin_work_log/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+from . import (
+    models,
+)
---
+++ ssi_voucher_mixin_work_log/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+{
+    "name": "Account Voucher Mixin - Work Log Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_voucher_mixin",
+        "ssi_work_log_mixin",
+    ],
+    "data": [],
+    "demo": [],
+    "images": [],
+}
